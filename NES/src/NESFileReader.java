d8f87754ddc0e52c761fa2c76022257819f38bed
William Nguyen <WDDNguyen@gmail.com>
2017-07-21 15:22:30 -0400

    added Log to track size of each memory and confirm the various type

--- NES/src/NESFileReader.java
+++ NES/src/NESFileReader.java
@@ -7,27 +7,28 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
+import javax.xml.bind.DatatypeConverter;
+
 public class NESFileReader {
 	
 	//reads INES (.nes) file format 
 	// NES uses little endian format 
+	//** Need to make sure the proper endian format is given to the header **
 	
-	//** Need to make sure the proper endian format is given to the header"
-	
-	// Header : 16 bytes
-	// Trainer -- if present (0 or 512 bytes)
-	// PRG_ROM -- required    (16384 * x bytes)
-	// CHR_ROM -- if present  (16384 * x bytes)
-	// INST_ROM -- if present (0 or 8192 bytes)
-	// PROM -- if present    (16 bytes Dad, 16 bytes Counter Out) often missing
-
 	//final int iNESIdentifier = 0x1A53354E; 
 	final static int iNESIdentifier = 0x4E45531a;
-	static byte[] headerByte = new byte[16];
-	
 	final static int MAX_FILE_SIZE = 65536;
 	final static String FILE_NAME = "D:/NESTEST/Tetris.nes";
 	final static String OUTPUT_FILE_NAME = "D:/NESTEST/binary.txt";
+	
+	static byte[] headerByte = new byte[16]; // Header : 16 bytes
+	static byte[] trainer = new byte[512];  		     // if present (0 or 512 bytes)
+	static byte[] PRGROM;   				 // required   (16384 * x units)
+	static byte[] CHRROM;   				 // if present (16384 * x units)
+	static byte[] PRGRAM;   				 // if present (0 or 8192 bytes)
+	static int mirrorType;					 // 0 : horizontal  1 : vertical
+	static int mapperType;					 // Mapper Number 
+	static boolean hasBattery;				 // Check if ROM uses an battery pack
 	
 	// testing retrieving data from iNES file
 	public static void main(String[] Args) throws IOException {
@@ -41,24 +42,41 @@ public class NESFileReader {
 			if(byteBuffer == null){
 				System.out.println("Byte Buffer is empty, did not load the file correctly");
 			}
-			
-			byteBuffer.order(ByteOrder.BIG_ENDIAN);
+				
+			byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
 			int byteRead = fc.read(byteBuffer);
-		
 			System.out.println("Number of bytes read : " + byteRead);
 			byteBuffer.flip();
 				
 			byteBuffer.get(headerByte,0, 16);
+			System.out.println("Byte buffer current position : " + byteBuffer.position());
 			
 			iNESHeader header = new iNESHeader(headerByte);
-			
 			if(headerByte.length != 16){
 				System.out.println("Header was not proprely loaded");
 			}
-			
 			if(header.identifier != iNESIdentifier){
 				System.out.println("This is not a iNES file (.nes)");
 			}
+			
+			if(header.hasTrainer()){
+				trainer = new byte[512];
+				byteBuffer.get(trainer, byteBuffer.position(), trainer.length);
+				System.out.println("Byte buffer current position : " + byteBuffer.position());
+			}
+				
+			PRGROM = new byte[header.getPRGROMSize()];
+			CHRROM = new byte[header.getCHRROMSize()];
+			PRGRAM = new byte[header.getPRGRAMSize()];
+			
+			byteBuffer.get(PRGROM, 0, header.getPRGROMSize());
+			System.out.println("Byte buffer current position : " + byteBuffer.position());
+			
+			byteBuffer.get(CHRROM, 0, header.getCHRROMSize());
+			System.out.println("Byte buffer current position : " + byteBuffer.position());
+		
+			byteBuffer.get(PRGRAM, 0, header.getPRGRAMSize());
+			System.out.println("Byte buffer current position : " + byteBuffer.position());
 			
 			byteBuffer.clear();
 			byteRead = fc.read(byteBuffer);
@@ -69,29 +87,22 @@ public class NESFileReader {
 	}
 	
 	public static void printLog(iNESHeader header){
+		System.out.println("--------------------HEADER------------------------");
 		System.out.println(Arrays.toString(headerByte));
-		System.out.println("Mirror Type : " + header.getMirrorType());
-		System.out.println("Battery Pack present : " + header.hasBatteryPack());
-		System.out.println("Trainer present : " + header.hasTrainer());
-		System.out.println("Mapper Type : " + header.getMapperType());
+		System.out.println("Mirror Type           : " + header.getMirrorType());
+		System.out.println("Battery Pack present  : " + header.hasBatteryPack());
+		System.out.println("Trainer present       : " + header.hasTrainer());
+		System.out.println("Mapper Type           : " + header.getMapperType());
 		System.out.println("PRG ROM SIZE in bytes : " + header.getPRGROMSize());
 		System.out.println("CHR ROM SIZE in bytes : " + header.getCHRROMSize());
 		System.out.println("PRG RAM SIZE in bytes : " + header.getPRGRAMSize());
+		System.out.println("---------------------MEMORY-----------------------");
+		System.out.println("Trainer Memory : " + DatatypeConverter.printHexBinary(trainer));
+		System.out.println("PRGROM Memory  : " + DatatypeConverter.printHexBinary(PRGROM));
+		System.out.println("PRGROM Memory  : " + DatatypeConverter.printHexBinary(CHRROM));
+		System.out.println("PRGROM Memory  : " + DatatypeConverter.printHexBinary(PRGRAM));
+		
+		
 		
 	}
-	
-	byte[] readBinaryFile(String aFileName) throws IOException {
-		Path path = Paths.get(aFileName);
-		return Files.readAllBytes(path);
-	}
-	
-	void writeBinaryFile(byte[] aBytes, String aFileName) throws IOException{
-		Path path = Paths.get(aFileName);
-		Files.write(path, aBytes);
-	}
-	
-	private static void log (Object aMsg){
-		System.out.println(String.valueOf(aMsg));
-	}
-	
 }
